4bfc7d4f4c712fa906cdfbfbec0a5f425a8c8e13
Andrew Kelton <109199143+AndrewKelton@users.noreply.github.com>
2024-12-26 00:16:36 -0500

    YOU CAN'T SEE THIS :)

--- backend/twitter_poster/path.py
+++ backend/twitter_poster/path.py
@@ -21,11 +21,11 @@ from outcome_checker import checkOutcome as cO
 # all of the same data in an object, keeps it
 # more organized
 class ApiKeys():
-    api_key = '9PpkkRCCWsuPpBmJLiBqmy0AM'
-    api_secret_key = 'cdIj6b37biYOeMGJZ3AmvdSyWtE06EbAb67wwzoiftg6u1L3eo'
-    access_token = '1681527848614371328-zHlMpTnsg8VyiRpB8YrssuE6vbQOT3'
-    access_secret_token = 'nFiD0etYvODX1xqNXr1WwMDLoYdI9XgULXy8bFfROTigr'
-    client_id = 'dGtDZXp0MjVodDBFb0hQTTk2dmU6MTpjaQ'
-    client_secret = '0VwVjWmw-oz0JdqDSkdLfuBuK5Kc1KN1u-EuMBKRrh_W3MpUfQ'
-    bearer_token = 'AAAAAAAAAAAAAAAAAAAAADppvQEAAAAABclQ6nuEDLooLQatIu4QLqMOxcA%3D9aZUECZIOhG2fWjuPsjKzulJvJAXtXCoKWFMuV822ghYP6bOUW'
+    api_key = '''YOU CAN'T SEE THIS :)'''
+    api_secret_key = '''YOU CAN'T SEE THIS :)'''
+    access_token = '''YOU CAN'T SEE THIS :)'''
+    access_secret_token = '''YOU CAN'T SEE THIS :)'''
+    client_id = '''YOU CAN'T SEE THIS :)'''
+    client_secret = '''YOU CAN'T SEE THIS :)'''
+    bearer_token = '''YOU CAN'T SEE THIS :)'''
     pass
