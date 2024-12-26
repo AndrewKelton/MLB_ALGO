22170e82cdc17b2074bfa9aab5ee052b654cfb22
Andrew Kelton <109199143+AndrewKelton@users.noreply.github.com>
2024-12-26 00:24:03 -0500

    Update check_innings_outcomes.py

--- backend/outcome_checker/check_innings_outcomes.py
+++ backend/outcome_checker/check_innings_outcomes.py
@@ -2,7 +2,7 @@ from backend import ExceptionsMLB
 from backend.outcome_checker import checkOutcome as cO
 
 
-#
+# CLEARLY INCOMPLETE
 
 def collect_picks(date):
     con, cur = cO.openDB("backend/data/innings.db")
